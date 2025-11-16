decd7b928ee860b6689c88db7925e781fa30eeb3
Ben4760 <476026333@tqstudent.edu.au>
2025-11-17 09:35:18 +1000

    Update homepage.bat

--- homepage.bat
+++ homepage.bat
@@ -1,24 +1,20 @@
 @echo off
-REM === Optional: Close all Edge windows to ensure changes take effect ===
-echo Closing all Microsoft Edge processes...
+REM Close Edge
 taskkill /IM msedge.exe /F >nul 2>&1
 
-REM === Set Edge homepage (GPO-Managed) ===
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v HomepageLocation /t REG_SZ /d "http://203.0.113.2:80" /f
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v HomepageIsNewTabPage /t REG_DWORD /d 0 /f
+REM Policies path
+set Key="HKLM\SOFTWARE\Policies\Microsoft\Edge"
 
-REM === Set Edge startup behaviour ===
-REM 4 = Open specific URLs
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v RestoreOnStartup /t REG_DWORD /d 4 /f
+REM Homepage
+REG ADD %Key% /v HomepageLocation /t REG_SZ /d "http://203.0.113.2:80" /f
+REG ADD %Key% /v HomepageIsNewTabPage /t REG_DWORD /d 0 /f
 
-REM === Startup URLs must be set in a subkey ===
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge\RestoreOnStartupURLs" /v "1" /t REG_SZ /d "http://203.0.113.2:80" /f
+REM Startup pages
+REG ADD %Key% /v RestoreOnStartup /t REG_DWORD /d 4 /f
+REG ADD "%Key%\RestoreOnStartupURLs" /v "1" /t REG_SZ /d "http://203.0.113.2:80" /f
 
-REM === Optional: Verify that the registry keys were written ===
-echo Verifying registry settings...
-REG QUERY "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v HomepageLocation
-REG QUERY "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v RestoreOnStartup
-REG QUERY "HKLM\SOFTWARE\Policies\Microsoft\Edge\RestoreOnStartupURLs" /v "1"
+REM New tab page (forces all new tabs to your URL)
+REG ADD %Key% /v NewTabPageLocation /t REG_SZ /d "http://203.0.113.2:80" /f
 
-echo Done. Microsoft Edge may need to be restarted for the changes to take effect.
+echo Done. Restart your computer to ensure changes apply.
 pause
